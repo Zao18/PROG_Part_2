b7e744707a3b64d08fd17eef57760f19dcf0a547
Zao18 <126759508+Zao18@users.noreply.github.com>
2024-10-18 21:39:44 +0200

    UnitTest 2 Added

---
+++ TestPROG/UnitTest2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PROG_Part_2.Controllers;
+using PROG_Part_2.Models;
+using PROG_Part_2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPROG
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void ApproveClaim_ValidId_UpdatesClaimStatus()
+        {
+            var fileServiceMock = new Mock<AzureFileShareService>("connectionString", "fileShareName");
+            var controller = new AdminController(fileServiceMock.Object);
+            var claim = new Claims
+            {
+                ClaimId = 1,
+                Name = "Zao",
+                HoursWorked = 40,
+                HourlyRate = 200,
+                Status = "Pending"
+            };
+
+            ClaimsController._claimsList.Add(claim);
+
+            var result = controller.ApproveClaim(1) as RedirectToActionResult;
+            Assert.NotNull(result);
+            Assert.Equal("PendingClaims", result.ActionName);
+            Assert.Equal("Approved", claim.Status);
+        }
+    }
+}
